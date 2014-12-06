8269f04b21cbcb602151b27486d48eae5d0089bc
EDUARDSI <eduards123>
2014-12-07 00:32:38 +0200

    extracted questions to questions.json

--- scripts/app.js
+++ scripts/app.js
@@ -2,27 +2,14 @@
 
 var app = angular.module('app', [])
 
-app.controller('AppController', ['$scope', function($scope) {
+app.controller('AppController', ['$scope', '$http', function($scope, $http) {
 
-  $scope.done = false	
-  $scope.chosenOption = null
+$http.get('questions.json').success(function(data) {
+	var todaysDate = $.formatDateTime('yy/mm/dd', new Date());
+    $scope.question = data[todaysDate]
+  });
+
   $scope.disableCheckButton = true
-
-  $scope.question = {
-		"title": "What DevOps really is?",
-		"options": [
-			{
-				"title": "IT position / role",
-				"text": "DevOps is a special guy in a team, usually referred as System Engineer, which is responsible for development tooling maintanence, improving development and ops communication, release management and much more",
-				"correct" : false
-			},
-			{
-				"title": "A set of practices",
-				"text": "DevOps is a set of practices which aims to enhance the software delivery by creating tighter links between the different IT people including Developers and Operations through the use of automation and communication.",
-				"correct" : true
-			}  	
-		]
-	}
 
 	$scope.chooseOption = function(index) {
 		if ($scope.done) {
@@ -38,12 +25,3 @@ app.controller('AppController', ['$scope', function($scope) {
 		$scope.showNotOk = !($scope.question.options[$scope.chosenOption].correct)
 	}
 }]);
-
-  // var correct = $(".list-group-item.active").hasClass('correct')
-
-  //       if (correct) {
-  //           $("#ok").removeClass("hide")
-  //       } else {
-  //           $("#notok").removeClass("hide")
-  //       }
-  //   });
